1a85f8aeeef8942bcab007580ab0ad28c6b2e78a
Nyou <luqianyou117@163.com>
2025-05-19 18:05:04 +0800

    登录用户信息更改相关页面设计

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, Link, Outlet, RouterProvider } from 'react-router-dom';
+import { Register } from './Register';
+import { Login } from './Login';
+import { UpdatePassword } from './UpdatePassword/UpdatePassword';
+import { ErrorPage } from './ErrorPage';
+import { Index } from './index/index';
+import { UpdateInfo } from './UpdateInfo/update_info';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import '@ant-design/v5-patch-for-react-19';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const routes = [
+    {
+        path: "/",
+        element: <Index></Index>,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: 'update_info',
+                element: <UpdateInfo />
+            },
+            {
+                path: 'bbb',
+                element: <div>bbb</div>
+            }
+        ]
+    },
+
+    { path: '/register', element: <Register /> },
+    { path: '/login', element: <Login /> },
+    { path: '/update_password', element: <UpdatePassword /> },
+]
+
+const router = createBrowserRouter(routes);
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(<RouterProvider router={router} />)
+
